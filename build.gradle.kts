2470e420e9f9f1157665938191968ac7b97208c9
404- <404-@users.noreply.github.com>
2023-04-02 18:46:54 -0400

    Update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm") version "1.8.20"
 }
 
 group = "fun.notfound"
@@ -13,12 +13,11 @@ repositories {
 
 dependencies {
     implementation("io.vavr:vavr:0.10.4")
-    implementation("io.arrow-kt:arrow-fx-coroutines:1.1.2")
-    implementation("io.arrow-kt:arrow-fx-stm:1.1.2")
+    implementation("io.arrow-kt:arrow-core:1.1.5")
 
     testImplementation(kotlin("test"))
-    testImplementation("com.google.code.gson:gson:2.10")
-    testImplementation("io.kotest:kotest-runner-junit5:5.5.4")
+    testImplementation("com.google.code.gson:gson:2.10.1")
+    testImplementation("io.kotest:kotest-runner-junit5:5.5.5")
 }
 
 tasks.test {
@@ -26,5 +25,10 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
